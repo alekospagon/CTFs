eda4d3deeb0cbc2203eb7bd7f97219fb6a3f1e57
alekospagon <56389540+alekospagon@users.noreply.github.com>
2019-12-26 09:07:06 -0500

    Update bof.c

--- pwnable.kr/bof_challenge/bof.c
+++ pwnable.kr/bof_challenge/bof.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
+
 void func(int key){
 	char overflowme[32];
 	printf("overflow me : ");
@@ -12,6 +13,7 @@ void func(int key){
 		printf("Nah..\n");
 	}
 }
+
 int main(int argc, char* argv[]){
 	func(0xdeadbeef);
 	return 0;
